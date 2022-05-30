46d305b86ca3d7e02b328ace929e791aa44319a3
chrispwolf <chrispwolf@gmail.com>
2022-05-30 12:42:55 -0400

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/campaigns',
+  //       destination: '/',
+  //       permanent: false,
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = nextConfig
